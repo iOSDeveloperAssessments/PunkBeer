d50fd9ede446e5e73d8585c81076f0372389aa68
David <momachilles@gmail.com>
2024-05-06 08:27:47 +0200

    Initial Commit

---
+++ PunkBeer/PunkBeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PunkBeerApp.swift
+//  PunkBeer
+//
+//  Created by David Alarcon on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct PunkBeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
